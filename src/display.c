6f2431b438a6cc96e4236ff67e385b4119e42af9
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-07-04 13:41:28 -0700

    Move version and usage to display.c

--- src/display.c
+++ src/display.c
@@ -1,2 +1,12 @@
 const char* usage_msg = "Usage: cproc [FILE]... [OPTION]...";
 const char* version_msg = "Version: 0.0.1";
+
+// To be called when -h or --help is invoked
+void usage() {
+	printf(usage_msg);
+}
+
+// To be called when -v or --version is invoked
+void version() {
+	printf(version_msg);
+}
--- src/main.c
+++ src/main.c
@@ -5,16 +5,6 @@
 #include <stdio.h>
 #include <display.h>
 
-// To be called when -h or --help is invoked
-void usage() {
-	printf(usage_msg);
-}
-
-// To be called when -v or --version is invoked
-void version() {
-	printf(version_msg);
-}
-
 int main(int argc, char** argv) {
 	version();
     return 0;
